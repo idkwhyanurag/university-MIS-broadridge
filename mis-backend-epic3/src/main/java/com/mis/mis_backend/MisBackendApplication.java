49c2a9eabb4feb488e8fdcd9ed9108d91f59f536
Anurag Majumdar <anurag.mj2008@gmail.com>
2026-08-06 14:49:33 +0530

    Split Epic 3 into its own module

---
+++ mis-backend-epic3/src/main/java/com/mis/mis_backend/MisBackendApplication.java
@@ -0,0 +1,11 @@
+package com.mis.mis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MisBackendApplication.class, args);
+    }
+}
